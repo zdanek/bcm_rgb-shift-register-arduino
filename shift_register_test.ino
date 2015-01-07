06f2f034bd6dc1e6050813209b193241c7e90623
Bartek Zdanowski <bartek.zdanowski@gmail.com>
2015-01-07 23:48:50 +0100

    working for attiny but too slow even on regs

--- shift_register_test.ino
+++ shift_register_test.ino
@@ -2,20 +2,20 @@
 //#include <mega16.h>
 
 //UNO
-#include "uno_consts.h"
+//#include "uno_consts.h"
 //ATTiny4313
-//#include "attiny4313_consts.h"
+#include "attiny4313_consts.h"
 
-int const clockPin = 4;    //PD2 - case pin 6
-int const latchPin = 5;    //PD3 - pin 7
-int const dataPin = 7;     //PD5 - pin 9
-int const outputEnablePin = 16;    //PD4 - pin 8
+//int const clockPin = 4;    //PD2 - case pin 6
+//int const latchPin = 5;    //PD3 - pin 7
+//int const dataPin = 7;     //PD5 - pin 9
+//int const outputEnablePin = 16;    //PD4 - pin 8
 
 //int const OUTPUT_BITS = 30;
 byte const FALSE_BIT = 0;
 byte const TRUE_BIT = 1;
 
-byte const MAX_SUPPORTED_BITS = 60;
+byte const MAX_SUPPORTED_BITS = 30;
 int const VALUE_LEVELS = 255;
 
 byte ledValues[MAX_SUPPORTED_BITS];
@@ -23,8 +23,8 @@ byte ledValues[MAX_SUPPORTED_BITS];
 #define PRODUCTION
 
 #ifndef PRODUCTION
-#define DEBUG(MSG) Serial.println(MSG)
-#define DEBUGN(MSG) Serial.print(MSG)
+#define DEBUG(MSG) Serial.println(F(MSG))
+#define DEBUGN(MSG) Serial.print(F(MSG))
 #define DEBUGHEX(LONG) Serial.println(LONG, HEX)
 #define DEBUGHEXN(LONG) Serial.print(LONG, HEX)
 #define DEBUG_BYTES(BYTES, LENGTH) { for (int dc = 0; dc < LENGTH; dc++) DEBUGHEXN(BYTES[dc]); DEBUG(""); }
@@ -46,7 +46,7 @@ const byte RECEIVING = 2;
 byte state = WAITING;
 byte dataPtr = 0;
 
-byte outputBits = 60;
+byte outputBits = 32;
 
 boolean received = false;  // whether the string is complete
 
@@ -71,36 +71,43 @@ void setup() {
      //BRG
      
         ledValues[0] = (byte) 0;
-        ledValues[1] = (byte) 1;
+        ledValues[1] = (byte) 2;
         ledValues[2] = (byte) 2;
         ledValues[3] = (byte) 2;
         ledValues[4] = (byte) 2;
         ledValues[5] = (byte) 2;
-        ledValues[6] = (byte) 1;
+        ledValues[6] = (byte) 2;
         ledValues[7] = (byte) 0;
                
         ledValues[8] = (byte) 0;
         ledValues[9] = (byte) 1;
-        ledValues[10] = (byte) 1;
-        ledValues[11] = (byte) 1;
-        ledValues[12] = (byte) 1;
-        ledValues[13] = (byte) 1;
+        ledValues[10] = (byte) 0;
+        ledValues[11] = (byte) 0;
+        ledValues[12] = (byte) 0;
+        ledValues[13] = (byte) 0;
         ledValues[14] = (byte) 1;
-        ledValues[15] = (byte) 1;
+        ledValues[15] = (byte) 0;
+        
+
         ledValues[16] = (byte) 0;
 
+        ledValues[28] = (byte) 0;
+        ledValues[29] = (byte) 1;
+        ledValues[30] = (byte) 0;
+        ledValues[31] = (byte) 0;
+
+
+/*
         for (int j=0; j < 5; j++) {
         for (int x=1;x < 7; x+=3) {
-            ledValues[j*8 + x] = (byte) 5;
-            ledValues[j*8 +x+1] = (byte) 5;
-            ledValues[j*8 +x+2] = (byte) 5;
+            ledValues[j*8 + x] = (byte) 1;
+            ledValues[j*8 +x+1] = (byte) 1;
+            ledValues[j*8 +x+2] = (byte) 1;
         }
         }
-
+*/
 
 }
-
-
 
 
 void loop() {
@@ -115,10 +122,10 @@ void loop() {
                         DATA_LOW;
                         DEBUG("D[0]");
                     }
-                    DELAY(500);
+                    DELAY(50);
                     CLOCK_HIGH;
                 DEBUG("   C[1]");                    
-                    DELAY(500);
+                    DELAY(50);
                     CLOCK_LOW;
                 DEBUG("   C[0]");
 
@@ -126,14 +133,14 @@ void loop() {
 
                 LATCH_HIGH;
                 DEBUG("       L[1]");
-                DELAY(1000)
+                DELAY(50)
                 LATCH_LOW;
                 DEBUG("       L[0]");
-                DELAY(1000);
+                DELAY(50);
                 
             }
 }
-
+/*
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
@@ -163,5 +170,5 @@ void serialEvent() {
         
   }
 }
-
+*/
 
